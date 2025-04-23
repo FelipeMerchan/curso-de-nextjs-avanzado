860c167e5152a1eeb68ba016f82189ab987fbfed
Felipe Merchan <felipemerchanfuquen@gmail.com>
2025-04-22 23:03:48 -0500

    Clase 4 de 24 - RSC: Refactorizando un componente cliente a servidor

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,22 +1,25 @@
-"use client"
-
-import { useEffect, useState } from "react"
-
 import { Heading, Text } from "@chakra-ui/react"
 
 import { Bookmark } from "@/components/bookmark"
 import { BookmarkType } from "./schema"
 
-export default function Bookmarks() {
-  const [bookmarks, setBookmarks] = useState<BookmarkType[]>([])
+/* Los React server components son una herramienta muy poderosa que tenemos
+que utilizar porque en performance y seo sone xcelentes, desde el primer
+momento en que el navegador recibe la primer llamada del htlm ya tenemos
+la mayor información disponible para darle al usuario,e s decir,
+hace un renderizado del lado del servidor. Esto no solamente mejora los
+tiempos de carga sino que también nos ayuda a que nuestro sitio sea mejor indexado
+por los navegadores y desde el primer render vamos a tener toda la inforamción disponible
+para el usuario de forma instantanea. */
 
-  useEffect(() => {
-    fetch("/bookmarks/api", {
-      next: { tags: ["bookmarks"] },
-    })
-      .then((response) => response.json() as Promise<{ data: BookmarkType[] }>)
-      .then(({ data }) => setBookmarks(data))
-  }, [])
+/* Para volver el componente un componente del servidor debemos
+volver el componente aincrono: */
+export default async function Bookmarks() {
+  /* Si trabajamos desde un server component las rutas relativas no existen
+  por lo cual no podemos usar await fetch("/bookmarks/api"), en su lugar,
+  debemos usar rutas absolutas como http://localhost:3000/bookmarks/api */
+  const bookmarksResponse = await fetch("http://localhost:3000/bookmarks/api")
+  const bookmarks = (await bookmarksResponse.json()) as { data: BookmarkType[] }
 
   return (
     <main className="mt-12">
@@ -30,7 +33,7 @@ export default function Bookmarks() {
       </header>
 
       <ul className="text-lg mt-10">
-        {bookmarks?.map((bookmark) => (
+        {bookmarks?.data.map((bookmark) => (
           <li className="border-b-2 py-4 px-6 my-2" key={bookmark.id}>
             <Bookmark {...bookmark} />
             {/* <div className="my-1 text-gray-600 text-xs ml-7">
