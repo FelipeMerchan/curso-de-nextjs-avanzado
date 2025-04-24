877ee31652282bcb08a79f647d0a51447f72bfe3
Felipe Merchan <felipemerchanfuquen@gmail.com>
2025-04-23 22:59:46 -0500

    Clase 7 de 24 - Consumiendo datos desde el cliente con React Query en Next.js 15

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import { Heading, Text } from "@chakra-ui/react"
 
 import { Bookmark } from "@/components/bookmark"
-import { orm } from "./db"
+import { useQuery } from "@tanstack/react-query"
+import { BookmarkType } from "./schema"
 
 /* Los React server components son una herramienta muy poderosa que tenemos
 que utilizar porque en performance y seo son excelentes, desde el primer
@@ -13,8 +16,8 @@ por los navegadores y desde el primer render vamos a tener toda la información 
 para el usuario de forma instantanea. */
 
 /* Para volver el componente un componente del servidor debemos
-volver el componente asíncrono: */
-export default async function Bookmarks() {
+volver el componente asíncrono (export default async function Bookmarks()): */
+export default function Bookmarks() {
   /* Si trabajamos desde un server component las rutas relativas no existen
   por lo cual no podemos usar await fetch("/bookmarks/api"), en su lugar,
   debemos usar rutas absolutas como http://localhost:3000/bookmarks/api */
@@ -35,12 +38,56 @@ export default async function Bookmarks() {
   orm. Las ORM nos permiten solucionar queries de forma más eficiente con nuestra
   base de datos.
   */
-  const bookmarks = await orm.query.bookmarks.findMany({
+  /* const bookmarks = await orm.query.bookmarks.findMany({
     limit: 10,
     with: {
       author: true,
     },
+  }) */
+
+  /* Consumo de datos desde el cliente */
+  /* Primera forma, con useState y useEffect */
+  /* const [bookmarks, setBookmarks] = useState<BookmarkType[]>([])
+  const [status, setStatus] = useState<
+    "iddle" | "loading" | "error" | "success"
+  >("iddle")
+
+  useEffect(() => {
+    setStatus("loading")
+    fetch("/bookmarks/api")
+      .then((response) => {
+        return response.json()
+      })
+      .then(({ data }) => {
+        setBookmarks(data)
+        setStatus("success")
+      })
+      .catch((error) => {
+        setStatus("error")
+      })
+  }, []) */
+
+  /* Segunda forma de consumir datos desde el cliente.
+  Con una librería muy buena como lo es React Query.
+  Si queremos hacer una app robusta debemos utilizar librerías
+  como react query para manejar todo lo asíncrono. */
+  const { data: bookmarks, status } = useQuery({
+    queryKey: ["bookmarks"], // identificador para caché
+    queryFn: async () => {
+      return fetch("/bookmarks/api")
+        .then((response) => {
+          return response.json()
+        })
+        .then(({ data }) => {
+          return data as BookmarkType[]
+        })
+        .catch((error) => {
+          console.log({ error })
+        })
+    },
   })
+
+  console.log({ status })
 
   return (
     <main className="mt-12">
